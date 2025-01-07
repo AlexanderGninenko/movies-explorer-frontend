6646f9311c2581f7b2f7a54fa28094e74998e3f9
Alexander <alexander.gninenko@icloud.com>
2025-01-07 12:02:19 +1000

    commit

--- src/utils/AuthAPI.js
+++ src/utils/AuthAPI.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://movies-explorer-api-2675.onrender.com/api';
+const BASE_URL = "https://movies-explorer-api-2675.onrender.com/api";
 // const BASE_URL = 'http://localhost:4010/api';
 
 const checkResponse = (res) => {
@@ -10,10 +10,10 @@ const checkResponse = (res) => {
 
 export const register = (name, password, email) => {
   return fetch(`${BASE_URL}/signup`, {
-    credentials: 'include',
-    method: 'POST',
+    credentials: "include",
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({ name, password, email }),
   }).then(checkResponse);
@@ -21,10 +21,10 @@ export const register = (name, password, email) => {
 
 export const authorization = (password, email) => {
   return fetch(`${BASE_URL}/signin`, {
-    credentials: 'include',
-    method: 'POST',
+    credentials: "include",
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({ password, email }),
   })
@@ -38,20 +38,20 @@ export const authorization = (password, email) => {
 
 export const signOut = () => {
   return fetch(`${BASE_URL}/signout`, {
-    credentials: 'include',
-    method: 'GET',
+    credentials: "include",
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   });
 };
 
 export const getMyUser = () => {
   return fetch(`${BASE_URL}/users/me`, {
-    credentials: 'include',
-    method: 'GET',
+    credentials: "include",
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   })
     .then(checkResponse)
@@ -60,9 +60,9 @@ export const getMyUser = () => {
 
 export const updateUserInfo = ({ name, email }) => {
   return fetch(`${BASE_URL}/users/me`, {
-    credentials: 'include',
-    method: 'PATCH',
-    headers: { 'Content-Type': 'application/json' },
+    credentials: "include",
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, email }),
   }).then(checkResponse);
 };
